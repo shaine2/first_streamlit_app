60d4915ce325f835663b622c148f0238658e3fff
shaine2 <142483074+shaine2@users.noreply.github.com>
2023-08-17 15:45:56 +0800

    Update streamlit_app.py
    edit line for menus

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,5 +4,5 @@ streamlit.title ('My Parents New Healthy Diner')
 
 streamlit.header ('Breakfast Menu')
 streamlit.text ('Omega 3 & Blueberrry Oatmeal')
-streamlit.txt ('Kale, Spinach & Rocket Smoothie')
-streamlit.txt('Hard-Boiled Free-Range Egg')
+streamlit.text ('Kale, Spinach & Rocket Smoothie')
+streamlit.text('Hard-Boiled Free-Range Egg')
