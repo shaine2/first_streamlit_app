779e2f997c278e7bd81327c20ece9b2dc1887d6a
shaine2 <142483074+shaine2@users.noreply.github.com>
2023-08-17 15:44:52 +0800

    Update streamlit_app.py
    edit line for breakfast menu

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,7 +2,7 @@ import streamlit
 
 streamlit.title ('My Parents New Healthy Diner')
 
-stremalit.header ('Breakfast Menu')
+streamlit.header ('Breakfast Menu')
 streamlit.text ('Omega 3 & Blueberrry Oatmeal')
 streamlit.txt ('Kale, Spinach & Rocket Smoothie')
 streamlit.txt('Hard-Boiled Free-Range Egg')
