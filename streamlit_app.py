6ba88534ca14a2377c6d7e97203b9ec5db7ec037
shaine2 <142483074+shaine2@users.noreply.github.com>
2023-08-21 23:17:00 +0800

    Update streamlit_app.py
    add load list values from streamlit

--- streamlit_app.py
+++ streamlit_app.py
@@ -46,3 +46,5 @@ streamlit.dataframe(my_data_rows)
 
 add_my_fruit = streamlit.text_input('What fruit would you like to add?','Kiwi')
 streamlit.write('Thank for adding ', add_my_fruit)
+
+my_cur.execute("inset into fruit_load_list values ('from streamlit')")
